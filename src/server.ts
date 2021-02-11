b4598d19f6c265c82aaa9a7fcc95c634ea15a6e3
Ulysses-Foglia <ulyfoglia@gmail.com>
2021-02-11 19:04:56 -0300

    First Commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => {
+  
+  // app.get('/', (request, response) => {
+  //   return response.json({ message: 'Hello GoStack' });
+  // });
+
+  console.log('*** Server started on port 3333! ***');
+});
